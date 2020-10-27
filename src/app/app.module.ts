d68f8ab0c1d1a66024f7c123b97b57b2f49a313e
Turi Gergő Marcell <turi.gergo.marcell@udinfopark.hu>
2020-10-27 09:19:11 +0100

    Login Module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MessageDialogComponent } from './shared/dialogs/message-dialog/message-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MessageDialogComponent
   ],
   imports: [
     BrowserModule,
