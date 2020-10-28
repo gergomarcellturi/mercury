568522d1fb4b0d1b7bcd0c1cb031a5be1da12866
Turi Gergő Marcell <turi.gergo.marcell@udinfopark.hu>
2020-10-28 17:38:23 +0100

    Firebase Configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import {TranslateService} from '@ngx-translate/core';
 import {environment} from '../environments/environment';
 import {FirebaseApp} from '@angular/fire';
 import {AngularFireDatabase} from '@angular/fire/database';
-import {Observable} from 'rxjs';
+import {Observable, pipe} from 'rxjs';
 import {User} from './api/dto/User';
 import {AngularFirestore} from '@angular/fire/firestore';
+import {map, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,7 @@ import {AngularFirestore} from '@angular/fire/firestore';
 export class AppComponent {
   title = 'mega-chad';
 
+  public chat: Observable<User[]>;
   public users: Observable<User[]>;
 
   constructor(
@@ -25,5 +27,9 @@ export class AppComponent {
   ) {
     translate.use(environment.defaultLang);
     this.db.collection('users').valueChanges().subscribe(console.log);
+    this.chat = this.db.collection('chat').valueChanges() as Observable<User[]>;
+    this.chat.forEach(console.log);
+    // this.chat = this.db.collection('chat').get();
+
   }
 }
