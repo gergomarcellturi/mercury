3968106228071f3ec4d59b379ab3da58394d7704
Pataki Donát <xpdonat@gmail.com>
2020-11-01 05:54:40 +0100

    initial chat

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {User} from '../../api/interfaces/User';
+import {ChatService} from '../../api/services/communication/chat.service';
+import {AuthenticationService} from '../../api/services/misc/authentication.service';
+import {environment} from '../../../environments/environment';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +13,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public chat: Observable<User[]>;
+  public users: Observable<User[]>;
+  public text: string;
+  public messages: {text: string, uid: string, from: string, timestamp: {seconds: number, nanoseconds: number}}[];
+
+  constructor(
+    public authenticationService: AuthenticationService,
+    private translate: TranslateService,
+    public chatService: ChatService,
+    ) {
+    translate.use(environment.defaultLang);
+    this.chatService.getChatLists().subscribe(response => {
+      this.messages = response[0].messages;
+      console.log(this.messages[3].timestamp);
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  public sendMessage = (): void => {
+    console.log(this.text);
+    if (!this.text) return;
+
+    this.chatService.sendMessage(this.text, '1uFaAU4p6ElQ3vEIkuAA');
+    this.text = '';
+  }
+
+  public isSamePerson = () => {
+
+  }
+
+  public onKeyPressed(event) {
+    if (event.key === 'Enter') {
+      this.sendMessage();
+    }
+  }
 }
