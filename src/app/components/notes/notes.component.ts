a3030eadcc75009a68847d43d278bf13ef7063f2
Pataki Donát <xpdonat@gmail.com>
2020-11-27 13:56:19 +0100

    notes styling

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-notes',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+
+  @Input() isStylish: boolean;
 
   constructor() { }
 
